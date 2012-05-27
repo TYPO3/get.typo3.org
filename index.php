8cbd4108a0882cff78affa1c5270c83aeebf0ba3
Fabien Udriot <fabien.udriot@ecodev.ch>
2012-05-27 17:16:07 +0200

    [TASK] Add downloadable file upon click + gerenal code improvement
    Change-Id: Ifec406ded8e129e9284ce262d073278989999ede

--- index.php
+++ index.php
@@ -2,85 +2,87 @@
 /**
  * CONFIG
  */
-$explanatoryFile = 'usage.html';
+$usageFile = 'usage.html';
+$sorryFile = 'sorry.html';
 $releasesFile = 'data/releases.txt';
 
-
+if ($_SERVER['REQUEST_URI'] == '/') {
+	// well... very there is room for improvement here ;)
+	$content = file_get_contents($usageFile);
+	print $content;
+	die();
+}
 
 /**
  * APPLICATION
  */
-
 $arguments = explode('/', $_SERVER['REDIRECT_SCRIPT_URL']);
 
-$requestedVersion = $arguments[1] ? $arguments[1] : 'stable';
-$requestedFormat = $arguments[2] ? $arguments[2] : 'tar.gz';
-if($requestedVersion == 'current') {
+$requestedVersion = !empty($arguments[1]) ? $arguments[1] : 'stable';
+$requestedFormat = !empty($arguments[2]) ? $arguments[2] : 'tar.gz';
+if ($requestedVersion == 'current') {
 	$requestedVersion = 'stable';
 }
 
+// Get information about version to download
 $redirectData = getRedirectUrl($requestedVersion, $requestedFormat, $releasesFile);
-if($redirectData === FALSE) {
-	echo '<p>Sorry, the requested package was not found.</p>
-	<p>
-		<a href="http://typo3.org/download/packages/">Official TYPO3 Packages</a><br />
-		<a href="http://get.typo3.org/">Explanation of this Short URL service</a>
-	</p>
-	';
+
+if (empty($redirectData)) {
+	// well... very there is room for improvement here ;)
+	$content = file_get_contents($sorryFile);
+	print $content;
+	die();
 } else {
-	$redirectUrl = $redirectData['url'];
-	$versionName = $redirectData['version'];
-	$formatName = $redirectData['format'];
-	if(
+	if (
 		strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'wget') === FALSE &&
 		strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'curl') === FALSE
 	) {
-		$downloadName = 'version <b>' . $versionName . '</b>';
-		if($formatName == 'zip') {
-			$downloadName .= ' in zip format';
-		}
-		$requestUrl = $_SERVER['REDIRECT_SCRIPT_URL'] == '/' ? '/current' : $_SERVER['REDIRECT_SCRIPT_URL'];
-
-		$content = file_get_contents($explanatoryFile);
-		$content = str_replace('###REDIRECTURL###', $redirectUrl, $content);
-		$content = str_replace('###TYPEURL###', 'get.typo3.org' . $requestUrl, $content);
-		$content = str_replace('###DOWNLOADNAME###', $downloadName, $content);
-		echo $content;die();
+		header("Content-type: application/octet-stream");
+		$fileName = basename($redirectData['url']);
+		header('Content-Disposition: attachment; filename="' . $fileName . '"');
+		readfile($redirectData['url']);
+		die();
 	} else {
-		header('Location: ' . $redirectUrl);die();
+		header('Location: ' . $redirectData['url']);
+		die();
 	}
 }
 
-
 function getRedirectUrl($versionName, $format, $releasesFile) {
+	$result = array();
 	$releases = json_decode(file_get_contents($releasesFile));
-	if($versionName == 'stable') {
+	if ($versionName == 'stable') {
 		$versionName = $releases->latest_stable;
 	}
-	if($versionName == 'dev') {
+	if ($versionName == 'dev') {
 		$versionName = getDevVersionName($releases);
 	}
 	$versionParts = explode('.', $versionName);
-	$branchName = $versionParts[0] . '.' . $versionParts[1];
-	$branch = $releases->$branchName;
-	if(!$versionParts[2]) {
-		$versionName = $branch->latest;
+
+	// Make sure we can retrieve a product release
+	if (count($versionParts) > 1 && in_array($format, array('tar.gz', 'zip'))) {
+		$branchName = $versionParts[0] . '.' . $versionParts[1];
+		$branch = $releases->$branchName;
+		if (!$versionParts[2]) {
+			$versionName = $branch->latest;
+		}
+
+		$version = $branch->releases->$versionName->version;
+		if ($version !== NULL) {
+			$result = array(
+				'url' => 'http://downloads.sourceforge.net/project/typo3/TYPO3%20Source%20and%20Dummy/TYPO3%20' . $version . '/typo3_src-' . $version . '.' . $format,
+				'version' => $version,
+				'format' => $format
+			);
+		}
 	}
-	$version = $branch->releases->$versionName->version;
-	if($version === NULL || !in_array($format, array('tar.gz', 'zip'))) {
-		return FALSE;
-	}
-	return array(
-		'url' => 'http://downloads.sourceforge.net/project/typo3/TYPO3%20Source%20and%20Dummy/TYPO3%20' . $version . '/typo3_src-' . $version . '.' . $format,
-		'version' => $version,
-		'format' => $format
-	);
+	return $result;
 }
 
 function getDevVersionName($releases) {
 	$latestBranch = '0.0';
-	foreach($releases as $key => $release) {
-		if(version_compare($key, $latestBranch) == 1) {
+	foreach ($releases as $key => $release) {
+		if (version_compare($key, $latestBranch) == 1) {
 			$latestBranch = $key;
 		}
 	}
