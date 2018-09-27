0ad3d7a261ca3bf246ed71bcfb4fdb4faa742820
tkretschmann <t.kretschmann@neusta.de>
2018-09-27 14:33:14 +0200

    [FEATURE] - add "toggle all" and "copy to clipboard" function

--- public/assets/JavaScript/composerHelper.js
+++ public/assets/JavaScript/composerHelper.js
@@ -7,7 +7,65 @@ document.addEventListener("DOMContentLoaded", function() {
 		return false;
 	});
 
+	fixedScrollOutputContainer(form);
+	toggleAll();
+	copyCommandToClipboard();
+
 });
+
+/**
+ * sets the container to fixed if it is about to scroll out of the viewport.
+ * @param form
+ */
+const fixedScrollOutputContainer = function(form) {
+	let ticking = false;
+	const outputContainer = document.getElementsByClassName('js-composer-helper-console');
+	const z = getContainerPosition(outputContainer[0]);
+
+	window.addEventListener('scroll', function(e) {
+		if(!ticking) {
+			window.requestAnimationFrame(function() {
+				if(this.scrollY !== 0 && this.scrollY >= z) {
+					const pos = outputContainer[0].getBoundingClientRect();
+					form.style.marginTop = pos.height + 'px';
+					outputContainer[0].classList.add('fixedTop');
+				} else {
+					form.style.marginTop = '0';
+					outputContainer[0].classList.remove('fixedTop');
+				}
+				ticking = false;
+			});
+			ticking = true;
+		}
+	});
+};
+
+const toggleAll = function() {
+	const toggleAllButton = document.getElementsByClassName('js-composer-helper-toggle-all');
+	const composerPackages = document.getElementsByClassName('js-composer-package-check');
+	let checkedAll = false;
+
+	toggleAllButton[0].addEventListener('click', function() {
+		Array.from(composerPackages).forEach(composerPackage => {
+			if(checkedAll === false) {
+				composerPackage.checked = true;
+				composerPackage.parentElement.classList.add('active');
+			} else {
+				composerPackage.checked = false;
+				composerPackage.parentElement.classList.remove('active');
+			}
+		});
+		checkedAll = !checkedAll;
+		checkboxChangeEvent();
+	});
+};
+
+const getContainerPosition = function(container) {
+	container.classList.remove('d-none');
+	const containerPosition = container.getBoundingClientRect();
+	container.classList.add('d-none');
+	return containerPosition.y + window.scrollY;
+};
 
 const vanillaAjaxForm = function(form) {
 	const formData = new FormData(form);
@@ -21,21 +79,21 @@ const vanillaAjaxForm = function(form) {
 		if(xhr.readyState === DONE) {
 			if(xhr.status === OK) {
 				const response = JSON.parse(xhr.responseText);
-				let outputContainerWrapper = document.getElementsByClassName('js-composer-helper-console');
-				let outputContainer = document.getElementsByClassName('js-composer-helper-packages');
-				outputContainer[0].innerHTML = '';
+				let outputContainerWrapper = document.getElementsByClassName('js-composer-helper-console')[0];
+				let outputContainer = document.getElementsByClassName('js-composer-helper-packages')[0];
+				outputContainer.innerHTML = '';
 
 				let showOutput = false;
-				Object.keys(response.status).forEach(function(composerPackage) {
+				Object.keys(response.status).forEach(composerPackage => {
 					if(response.status[composerPackage] === true) {
 						showOutput = true;
-						outputContainer[0].insertAdjacentText('beforeend', ' ' + composerPackage)
+						outputContainer.insertAdjacentText('beforeend', ' ' + composerPackage)
 					}
 				});
 
-				outputContainerWrapper[0].classList.add('d-none');
+				outputContainerWrapper.classList.add('d-none');
 				if(showOutput) {
-					outputContainerWrapper[0].classList.remove('d-none');
+					outputContainerWrapper.classList.remove('d-none');
 				}
 			} else {
 				console.error('Composer Helper Error: ' + xhr.status + ' - ' + xhr.statusText);
@@ -44,8 +102,33 @@ const vanillaAjaxForm = function(form) {
 	};
 };
 
+/**
+ * This event is assigned via the onChange attribute since bootstraps data-toggle for checkbox buttons suppresses the change event
+ */
 const checkboxChangeEvent = function() {
 	const form = document.getElementById('js-composer-helper-form');
 	const event = new Event('submit');
 	form.dispatchEvent(event);
+};
+
+const copyCommandToClipboard = function() {
+	const copyToClipboardButton = document.getElementsByClassName('js-command-copy-clipboard')[0];
+	copyToClipboardButton.addEventListener('click', function() {
+		const selection = window.getSelection();
+		const range = document.createRange();
+		const command = document.getElementsByClassName('js-composer-command')[0];
+		range.selectNodeContents(command);
+		selection.removeAllRanges();
+		selection.addRange(range);
+
+		document.execCommand('copy');
+		selection.removeAllRanges();
+
+		const original = copyToClipboardButton.textContent;
+		copyToClipboardButton.textContent = 'Copied!';
+
+		setTimeout(() => {
+			copyToClipboardButton.textContent = original;
+		}, 1200);
+	})
 };
