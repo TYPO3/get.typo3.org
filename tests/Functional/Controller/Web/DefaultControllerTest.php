5ada544e87ee258c69ec27ad6be16b8fd593223d
Benjamin Kott <benjamin.kott@outlook.com>
2019-11-22 16:10:07 +0100

    [TASK] Add functional tests for version routes

--- tests/Functional/Controller/Web/DefaultControllerTest.php
+++ tests/Functional/Controller/Web/DefaultControllerTest.php
@@ -16,23 +16,106 @@ use App\Tests\Functional\AbstractCase;
 
 class DefaultControllerTest extends AbstractCase
 {
-    /**
-     * @test
-     */
-    public function webDefaultRoute()
+    public function setUp(): void
     {
+        parent::setUp();
         $this->addFixture(new MajorVersionFixtures());
         $this->addFixture(new ReleaseFixtures());
         $this->addFixture(new RequirementFixtures());
         $this->executeFixtures();
+    }
 
+    /**
+     * @test
+     */
+    public function webDefault()
+    {
         $this->client->request('GET', '/');
-
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Build Blazingly');
         $this->assertSelectorTextContains('#download-community-1 .btn', 'Get version 10');
         $this->assertSelectorTextContains('#download-community-2 .btn', 'Get version 9');
         $this->assertSelectorTextContains('#download-elts-1 .btn-primary', 'Buy ELTS');
         $this->assertSelectorTextNotContains('h4', 'TYPO3 4.5');
+    }
+
+    /**
+     * @test
+     */
+    public function webDownloadRedirect()
+    {
+        $this->client->request('GET', '/download');
+        $this->assertResponseRedirects();
+        $this->assertSelectorTextContains('a', '/version/10');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionSprint()
+    {
+        $this->client->request('GET', '/version/10');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'TYPO3 10');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionSpecific()
+    {
+        $this->client->request('GET', '/version/10.0.0');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', '(10.0.0)');
+        $this->assertSelectorNotExists('#notice-elts');
+        $this->assertSelectorExists('#accordion-download');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionElts()
+    {
+        $this->client->request('GET', '/version/6.2');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', '(6.2.23 ELTS)');
+        $this->assertSelectorExists('#notice-elts');
+        $this->assertSelectorNotExists('#accordion-download');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionBeforeElts()
+    {
+        $this->client->request('GET', '/version/6.2.0');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', '(6.2.0)');
+        $this->assertSelectorExists('#notice-elts');
+        $this->assertSelectorExists('#accordion-download');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionOutdated()
+    {
+        $this->client->request('GET', '/version/4.5.0');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', '(4.5.0)');
+        $this->assertSelectorExists('#notice-outdated');
+        $this->assertSelectorExists('#accordion-download');
+    }
+
+    /**
+     * @test
+     */
+    public function webVersionOutdatedElts()
+    {
+        $this->client->request('GET', '/version/4.5');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', '(4.5.23 ELTS)');
+        $this->assertSelectorExists('#notice-outdated');
+        $this->assertSelectorNotExists('#accordion-download');
     }
 }
