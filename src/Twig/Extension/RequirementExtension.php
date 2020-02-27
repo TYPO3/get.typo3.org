7ac2b7ee69fa49256615e1f6b9f9da22c1e2f906
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-02-27 12:52:28 +0100

    [TASK] Add sorting and version normalization to extension

---
+++ src/Twig/Extension/RequirementExtension.php
@@ -0,0 +1,131 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3o/gettypo3org.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Twig\Extension;
+
+use App\Entity\Requirement;
+use App\Enum\RequirementCategoryEnum;
+use App\Utility\VersionUtility;
+use Doctrine\Common\Collections\Collection;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class RequirementExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            // format filters
+            new TwigFilter('formatVersions', [$this, 'formatVersions']),
+
+            // sorting filters
+            new TwigFilter('sortByName', [$this, 'sortByName']),
+
+            // group and sorting filters
+            new TwigFilter('groupByCategory', [$this, 'groupByCategory']),
+
+            // combined filters
+            new TwigFilter('prepareRequirements', [$this, 'prepareRequirements']),
+        ];
+    }
+
+    public function formatVersions($data): array
+    {
+        if ($data instanceof Collection) {
+            $elements = $data->toArray();
+        } elseif (\is_array($data)) {
+            $elements = $data;
+        } else {
+            throw new \Exception('Unsupported type for data: ' . gettype($data));
+        }
+
+        if (reset($elements) instanceof Requirement) {
+            $this->normalizeVersionHelper($elements);
+        } else {
+            foreach ($elements as &$category) {
+                $this->normalizeVersionHelper($category);
+            }
+        }
+
+        return $elements;
+    }
+
+    public function groupByCategory($data): array
+    {
+        if ($data instanceof Collection) {
+            $elements = $data->toArray();
+        } elseif (\is_array($data)) {
+            $elements = $data;
+        } else {
+            throw new \Exception('Unsupported type for data: ' . gettype($data));
+        }
+
+        $result = [];
+        foreach ($elements as $element) {
+            $result[$element->getCategory()][] = $element;
+        }
+        return $result;
+    }
+
+    public function prepareRequirements(Collection $data): array
+    {
+        $result = $this->formatVersions($data);
+        $result = $this->groupByCategory($result);
+        $result = $this->sortByName($result);
+
+        return $result;
+    }
+
+    public function sortByName($data): array
+    {
+        if ($data instanceof Collection) {
+            $elements = $data->toArray();
+        } elseif (\is_array($data)) {
+            $elements = $data;
+        } else {
+            throw new \Exception('Unsupported type for data: ' . gettype($data));
+        }
+
+        if (reset($elements) instanceof Requirement) {
+            $this->sortByNameHelper($elements);
+        } else {
+            foreach ($elements as &$category) {
+                $this->sortByNameHelper($category);
+            }
+        }
+
+        return $elements;
+    }
+
+    private function normalizeVersionHelper(array &$data)
+    {
+        foreach ($data as &$requirement) {
+            if (
+                \in_array(
+                    $requirement->getCategory(),
+                    [
+                        RequirementCategoryEnum::OPTION_PHP,
+                        RequirementCategoryEnum::OPTION_DATABASE,
+                    ]
+                )
+            ) {
+                $requirement->setMin(VersionUtility::normalize($requirement->getMin()));
+                $requirement->setMax(VersionUtility::normalize($requirement->getMax()));
+            }
+        }
+    }
+
+    private function sortByNameHelper(array &$data)
+    {
+        usort($data, function ($a, $b) {
+            return strcasecmp($a->getName(), $b->getName());
+        });
+    }
+}
