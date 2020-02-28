57265ae132ccfa1594f37f0d260c62fd1f237fb6
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-02-28 13:33:54 +0100

    [TASK] Remove deprecated wiki link

---
+++ src/Twig/Extension/ReleaseNotesExtension.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3o/gettypo3org.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Twig\Extension;
+
+use App\Entity\Requirement;
+use App\Enum\RequirementCategoryEnum;
+use App\Utility\VersionUtility;
+use Doctrine\Common\Collections\Collection;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ReleaseNotesExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            // format filters
+            new TwigFilter('removeWikiLink', [$this, 'removeWikiLink']),
+        ];
+    }
+
+    public function removeWikiLink(string $data): string
+    {
+        return preg_replace('/\(TYPO3_CMS_[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} \\"wikilink\\"\)/', '', $data);
+    }
+}
