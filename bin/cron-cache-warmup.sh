6805434343b5b329175f88674126211fdac43c70
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-03-17 00:15:43 +0100

    [TASK] Write cache warmup output to log

--- bin/cron-cache-warmup.sh
+++ bin/cron-cache-warmup.sh
@@ -2,13 +2,14 @@
 
 LOCKFILE=~/cache-warmup.lock
 STARTFILE=~/cache-warmup.run
+LOGFILE=~/cache-warmup.log
 
 if [ -f "$LOCKFILE" ]; then
     echo "Lockfile $LOCKFILE present, skipping…"
 elif [ -f "$STARTFILE" ]; then
     touch $LOCKFILE
     rm -f $STARTFILE
-    php ~/site/mage/current/bin/console cache:warmup
+    php ~/site/mage/current/bin/console cache:warmup > $LOGFILE
     rm -f $LOCKFILE
 elif [ "$1" == 'start' ]; then
     touch $STARTFILE
