1adac47d1340fced08fdb842b03b2af49f9157a7
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-03-16 23:53:27 +0100

    [TASK] Extend cron cache warmup

--- bin/cron-cache-warmup.sh
+++ bin/cron-cache-warmup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-if [ -f ~/.force-cache-warmup ]; then
-    rm ~/.force-cache-warmup
+LOCKFILE=~/cache-warmup.lock
+STARTFILE=~/cache-warmup.run
+
+if [ -f "$LOCKFILE" ]; then
+    echo "Lockfile $LOCKFILE present, skipping…"
+elif [ -f "$STARTFILE" ]; then
+    touch $LOCKFILE
+    rm -f $STARTFILE
     php ~/site/mage/current/bin/console cache:warmup
+    rm -f $LOCKFILE
+elif [ "$1" == 'start' ]; then
+    touch $STARTFILE
 fi
