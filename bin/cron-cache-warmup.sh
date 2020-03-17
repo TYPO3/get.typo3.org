f7a3f6e9529f6350e4d948c2166cd6346ce8da56
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-03-17 17:45:03 +0100

    [TASK] Capture all output of cron cache warmup

--- bin/cron-cache-warmup.sh
+++ bin/cron-cache-warmup.sh
@@ -9,7 +9,7 @@ if [ -f "$LOCKFILE" ]; then
 elif [ -f "$STARTFILE" ]; then
     touch $LOCKFILE
     rm -f $STARTFILE
-    php ~/site/mage/current/bin/console cache:warmup > $LOGFILE
+    php ~/site/mage/current/bin/console cache:warmup &> $LOGFILE
     rm -f $LOCKFILE
 elif [ "$1" == 'start' ]; then
     touch $STARTFILE
