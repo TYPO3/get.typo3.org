4951840d1224074929c55a7486eda2242083dec7
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-03-19 09:00:20 +0100

    [TASK] Remove lock file in case of hanging

--- satis/buildall.sh
+++ satis/buildall.sh
@@ -2,11 +2,11 @@
 DEBUG=yes
 if [ "$1" == '-q' ]
 then
-	DEBUG=
+    DEBUG=
 fi
 if [ -n "$DEBUG" ]
 then
-	set -x
+    set -x
 fi
 
 if [ -n "$I1S_DDEV_PROJECT" ] ; then
@@ -20,31 +20,32 @@ WEB_DIR="`dirname \"$0\"`/../../../../shared/public"
 WEB_DIR="`( cd \"$WEB_DIR\" && pwd )`"
 
 if [ -f "$LOCKFILE" ]; then
-	# Find out if the process is more than 1 hour old.
-	if [ -n "$(find $LOCKFILE -mmin +60)" ]; then
-		OLDPID=$(pgrep -o -f "buildall.sh")
-		# In this case we assume that it's hanging, the task will be killed.
-		echo "Process $OLDPID seems to be hanging. Killing it." >&2
-		kill $OLDPID
-	else
-		echo "Lockfile $LOCKFILE present, skipping…"
-		exit 0
-	fi
+    # Find out if the process is more than 1 hour old.
+    if [ -n "$(find $LOCKFILE -mmin +60)" ]; then
+        OLDPID=$(pgrep -o -f "buildall.sh")
+        rm -f $LOCKFILE
+        # In this case we assume that it's hanging, the task will be killed.
+        echo "Process $OLDPID seems to be hanging. Killing it." >&2
+        kill $OLDPID
+    else
+        echo "Lockfile $LOCKFILE present, skipping…"
+        exit 0
+    fi
 fi
 touch $LOCKFILE
 cd $APP_DIR/package-generator
 $APP_DIR/package-generator/Build.sh
 if [ -n "$DEBUG" ]
 then
-	$APP_DIR/package-generator/bin/typo3-cms-package-generator satis:json:create
+    $APP_DIR/package-generator/bin/typo3-cms-package-generator satis:json:create
 else
-	$APP_DIR/package-generator/bin/typo3-cms-package-generator satis:json:create > /dev/null
+    $APP_DIR/package-generator/bin/typo3-cms-package-generator satis:json:create > /dev/null
 fi
 if [ -n "$DEBUG" ]
 then
-	php -d memory_limit=-1 $APP_DIR/package-generator/bin/satis build $APP_DIR/package-generator/satis.json $WEB_DIR --skip-errors
+    php -d memory_limit=-1 $APP_DIR/package-generator/bin/satis build $APP_DIR/package-generator/satis.json $WEB_DIR --skip-errors
 else
-	php -d memory_limit=-1 $APP_DIR/package-generator/bin/satis build $APP_DIR/package-generator/satis.json $WEB_DIR --skip-errors > /dev/null 2>&1
+    php -d memory_limit=-1 $APP_DIR/package-generator/bin/satis build $APP_DIR/package-generator/satis.json $WEB_DIR --skip-errors > /dev/null 2>&1
 fi
 # Exchange index
 mv $WEB_DIR/index.html $WEB_DIR/satis.html
@@ -52,5 +53,5 @@ mv $WEB_DIR/index.html $WEB_DIR/satis.html
 rm -f $LOCKFILE
 if [ -n "$DEBUG" ]
 then
-	set -x
+    set -x
 fi
