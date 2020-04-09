9350ad9401693f5f6358dcdba62abcff1d0879bd
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-04-09 14:30:36 +0200

    [TASK] Optimize Satis build script (#138)

--- satis/build.sh
+++ satis/build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# Output options
-if [ "$1" == '-q' ]; then
-    DEBUG=
-else
-    DEBUG=yes
-fi
+# Defaults
+ALL=
+DEBUG=yes
 
+# Parse arguments
+for arg in $@
+do
+    case $arg in
+        -q)
+            DEBUG=
+            ;;
+        -a)
+            ALL=yes
+            ;;
+        *)
+            echo "Unknown parameter '$arg'"
+            ;;
+    esac
+done
+
+# Output options
 if [ -n "$DEBUG" ]; then
     set -x
 fi
@@ -25,7 +39,7 @@ fi
 WEB_DIR="`( cd \"$WEB_DIR\" && pwd )`"
 
 # Check for already running processes
-CURPID=$(pgrep -n -f ${0##*/})
+CURPID=$$
 
 if [ -f "$LOCKFILE" ]; then
     # Check if the process is more than 1 hour old.
@@ -51,9 +65,9 @@ BIN_DIR=$APP_DIR/bin
 
 cd $WEB_DIR/../satis
 
-# Run full build once a day only at 5am
+# Run full build once a day only at 3 am or on request
 CURRENT_TIME=$(date +%H:%M)
-if [[ "$CURRENT_TIME" > "03:00" ]] && [[ "$CURRENT_TIME" < "03:01" ]]; then
+if [ "$CURRENT_TIME" = "03:00" ] || [ -n "$ALL" ]; then
     BUILD_PARAM=--all
 else
     BUILD_PARAM=
@@ -61,18 +75,9 @@ fi
 
 if [ -n "$DEBUG" ]; then
     php -d memory_limit=-1 $BIN_DIR/typo3-cms-package-generator satis:build $WEB_DIR $BUILD_PARAM
-    #$BIN_DIR/typo3-cms-package-generator extensions:ter:json:create
-    #$BIN_DIR/typo3-cms-package-generator satis:json:create
-    #php -d memory_limit=-1 $BIN_DIR/satis build ./satis.json $WEB_DIR --skip-errors
 else
     php -d memory_limit=-1 $BIN_DIR/typo3-cms-package-generator satis:build $WEB_DIR $BUILD_PARAM > /dev/null
-    #$BIN_DIR/typo3-cms-package-generator extensions:ter:json:create > /dev/null
-    #$BIN_DIR/typo3-cms-package-generator satis:json:create > /dev/null
-    #php -d memory_limit=-1 $BIN_DIR/satis build ./satis.json $WEB_DIR --skip-errors > /dev/null
 fi
-
-# Rename Satis index
-#mv $WEB_DIR/index.html $WEB_DIR/satis.html
 
 # Remove lock file
 rm -f $LOCKFILE
