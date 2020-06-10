a5ca5348b8c7b113a47c4a7eff06b84d9c217bca
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-06-10 19:02:26 +0200

    [TASK] Open commit links in blank target (#174)

--- src/Twig/Filter/CommitLinker.php
+++ src/Twig/Filter/CommitLinker.php
@@ -29,7 +29,10 @@ class CommitLinker extends AbstractExtension
         return preg_replace_callback(
             $pattern,
             function (array $matches): string {
-                return sprintf('<a href="https://github.com/TYPO3/TYPO3.CMS/commit/%1$s">%1$s</a>', $matches['commit']);
+                return sprintf(
+                    '<a href="https://github.com/TYPO3/TYPO3.CMS/commit/%1$s" target="_blank" rel="noopener">%1$s</a>',
+                    $matches['commit']
+                );
             },
             $changeList
         );
