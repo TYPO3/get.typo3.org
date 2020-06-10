6c230f1cdb6924d887f5c42743b37cd3498c0c9a
Mathias Brodala <mbrodala@pagemachine.de>
2020-06-10 18:36:49 +0200

    Link commits in release notes changes (#173)
    * [FEATURE] Link commits in release notes changes
    
    Fixes #172
    
    * [BUGFIX] Add missing quote and change filter order
    
    Co-authored-by: Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>

---
+++ src/Twig/Filter/CommitLinker.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3o/gettypo3org.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace App\Twig\Filter;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class CommitLinker extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('commitlinker', [$this, 'linkCommits']),
+        ];
+    }
+
+    public function linkCommits(string $changeList): string
+    {
+        $pattern = '#(?<commit>[a-f0-9]{10,40})#m';
+
+        return preg_replace_callback(
+            $pattern,
+            function (array $matches): string {
+                return sprintf('<a href="https://github.com/TYPO3/TYPO3.CMS/commit/%1$s">%1$s</a>', $matches['commit']);
+            },
+            $changeList
+        );
+    }
+}
