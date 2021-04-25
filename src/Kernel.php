adda10e2b1a1f475de12ebec4b944d43316e8dee
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2021-04-25 21:53:19 +0200

    [TASK] Change package name (#243)

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the package t3o/gettypo3org.
+ * This file is part of the package t3o/get.typo3.org.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
