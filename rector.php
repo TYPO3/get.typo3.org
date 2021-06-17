8c7a6593ec932ed363c4ed9c9ba05f43561bfc39
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2021-06-17 18:05:10 +0200

    [TASK] Add Rector paths to configuration

--- rector.php
+++ rector.php
@@ -11,6 +11,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
 
+    // paths to refactor; solid alternative to CLI arguments
+    $parameters->set(Option::PATHS, [__DIR__ . '/src'/*, __DIR__ . '/tests'*/]);
+
+    // Run Rector only on changed files
+    $parameters->set(Option::ENABLE_CACHE, true);
+    //$parameters->set(Option::CACHE_DIR, __DIR__ . '/tmp/rector_cached_files');
+
+    // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
+    //$parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
+
     // Define what rule sets will be applied
     $containerConfigurator->import(SetList::DEFLUENT);
     $containerConfigurator->import(SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION);
