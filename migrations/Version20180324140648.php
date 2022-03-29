d05aef00cc0b7f8ea468ebdccceac682cbfdbcc7
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2022-03-29 20:01:59 +0200

    [TASK] Upgrade doctrine/dbal and dependencies to 3.x (#302)

--- migrations/Version20180324140648.php
+++ migrations/Version20180324140648.php
@@ -1,10 +1,24 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 /*
- * This file is part of the package t3o/gettypo3org.
+ * This file is part of the package t3o/get.typo3.org.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
 namespace DoctrineMigrations;
@@ -20,7 +34,10 @@ class Version20180324140648 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', "Migration can only be executed safely on 'sqlite'.");
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
 
         $this->addSql('CREATE TABLE major_version (version DOUBLE PRECISION NOT NULL, title VARCHAR(255) NOT NULL, subtitle VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, maintained_until DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
         , release_date DATETIME NOT NULL --(DC2Type:datetime_immutable)
@@ -34,7 +51,10 @@ class Version20180324140648 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', "Migration can only be executed safely on 'sqlite'.");
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
 
         $this->addSql('DROP TABLE major_version');
         $this->addSql('DROP TABLE "release"');
--- migrations/Version20191112143111.php
+++ migrations/Version20191112143111.php
@@ -3,10 +3,22 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package t3o/gettypo3org.
+ * This file is part of the package t3o/get.typo3.org.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
  */
 
 namespace DoctrineMigrations;
@@ -19,23 +31,29 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20191112143111 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'Add ELTS bool to releases';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', "Migration can only be executed safely on 'sqlite'.");
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
 
         $this->addSql("ALTER TABLE release ADD COLUMN elts BOOLEAN DEFAULT '0' NOT NULL");
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', "Migration can only be executed safely on 'sqlite'.");
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
+        );
 
         $this->addSql('DROP INDEX IDX_9E47031D17E84B00');
         $this->addSql('CREATE TEMPORARY TABLE __temp__release AS SELECT version, major_version, date, type, tar_package_md5sum, tar_package_sha1sum, tar_package_sha256sum, zip_package_md5sum, zip_package_sha1sum, zip_package_sha256sum, release_notes_news_link, release_notes_news, release_notes_upgrading_instructions, release_notes_changes, release_notes_legacy_content FROM "release"');
